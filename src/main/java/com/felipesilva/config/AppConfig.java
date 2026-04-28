d099515a788af0cb64fb9cb4fea0d8d632d6db3c
Felipe-Serra-Silva <felipeserrasilva.com@gmail.com>
2026-04-27 21:00:16 -0300

    add AppConfig to load configuration from properties file

---
+++ src/main/java/com/felipesilva/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.felipesilva.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class AppConfig {
+    private String recipient;
+    private String smtpHost;
+    private int smtpPort;
+    private String smtpUser;
+    private String smtpPassword;
+
+    public AppConfig(String configPath) throws  IOException{
+        Properties props = new Properties();
+
+        props.load(new FileInputStream(configPath));
+
+        this.recipient = props.getProperty("email.recipient");
+        this.smtpHost = props.getProperty("smtp.host");
+        this.smtpPort = Integer.parseInt(props.getProperty("smtp.port"));
+        this.smtpUser = props.getProperty("smtp.user");
+        this.smtpPassword = props.getProperty("smtp.password");
+    }
+
+    public String getRecipient() { return recipient; }
+    public String getSmtpHost() { return smtpHost; }
+    public int getSmtpPort() { return smtpPort; }
+    public String getSmtpUser() { return smtpUser; }
+    public String getSmtpPassword() { return smtpPassword; }
+}
